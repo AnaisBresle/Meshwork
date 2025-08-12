d53e09ddd441e6c4e88b20fe819fe1f43979b458
AnaisBresle <anais.bresle@gmail.com>
2025-08-12 21:10:27 +0100

    start working on interactions - need to look at enum closer.

--- server/models/interaction.js
+++ server/models/interaction.js
@@ -6,16 +6,44 @@ const Interaction = sequelize.define("Interaction", {
     type: DataTypes.INTEGER,
     primaryKey: true
   },
-  interactionType: {
-    type: DataTypes.ENUM('thanks', 'like'),
+
+  userId: {
+    type: DataTypes.INTEGER,
     allowNull: false,
-    unique: true,
+    references: {
+      model: "User", 
+      key: "id",
+    },
+    onDelete: "CASCADE",
   },
- 
+
+  postId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Post", 
+      key: "id",
+    },
+    onDelete: "CASCADE",
+  },
+
+
   ///////////////to revisit - unfinished
+ interactionType: {
+      type: DataTypes.ENUM("thanks", "like"),
+      allowNull: false,
+    },
+ 
+  
 }, {
   tableName: "interactions",
-  timestamps: false // does not add createdAt and updatedAt
+  timestamps: false, // does not add createdAt and updatedAt
+  indexes: [
+  {
+    unique: true,
+    fields: ["userId", "postId", "interactionType"], // unique combination - so user can ony have 1 interaction with 1 post. 
+  },
+],
 });
 
 module.exports = Interaction;
