0f9afa0b692993462e315b50b45ee9fe0bb2ed3d
AnaisBresle <anais.bresle@gmail.com>
2025-08-18 09:41:21 +0100

    update so include newPostslist component

--- client/src/pages/MainFeed.js
+++ client/src/pages/MainFeed.js
@@ -1,37 +1,11 @@
 
 import { useOutletContext } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 
 export default function MainFeed() {
   // filters provided by <Outlet context={{ filters }} />
   const { filters } = useOutletContext(); // { category, industry, sort }
-  const [posts, SetPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null); // Declaring error
-
-  useEffect(() => {
-    fetch("http://localhost:3001/api/posts") 
-      .then((res) => {
-        if (!res.ok) throw new Error("Network response was not ok");
-        return res.json();
-      })
-      .then((data) => {
-        setPosts(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
-  }, []);
-
-
-   // loader UI
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
-
+  
   // Start with everything
   let list = [...posts];
 
@@ -57,13 +31,7 @@ export default function MainFeed() {
   return (
     <div>
       <h1>Main Feed</h1>
-      <ul>
-        {list.map((post) => (
-          <li key={post.id}>
-            <strong>{post.title}</strong> — {post.category} · {post.industry} · {post.date}
-          </li>
-        ))}
-      </ul>
+    <PostsList filters={filters} />
     </div>
   );
 }
