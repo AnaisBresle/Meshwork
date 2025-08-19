ab08aed7d2aaa84be079e5e7a2d1daa5bef66339
AnaisBresle <anais.bresle@gmail.com>
2025-08-19 19:16:36 +0100

    base on event

---
+++ client/src/components/NewPost.jsx
@@ -0,0 +1,210 @@
+import { useState } from "react";
+import { useSession } from "../contexts/SessionContext";
+import {
+  LinkIcon,
+  PhotoIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/24/outline";
+
+const NewPost = () => {
+  const { user, token } = useSession();
+
+  if (!user) return <p>Loading user info...</p>;
+
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+      
+    topicId: {
+
+    parentId:      
+    
+    
+
+  const displayError = (message) => {
+    setError(message);
+    setTimeout(() => {
+      setError('');
+    }, 3000);
+  };
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+    // Validations
+  if (!title || !description || !type || !eventDate || !eventTime) {
+    displayError('Please fill in all required fields.');
+    return;
+  }
+
+
+  const newPostData = {
+    title,
+    content,
+    userId: user.id
+  };
+
+  try {
+      const response = await fetch("http://localhost:3001/api/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json",
+      'Authorization': `Bearer ${token}`,  
+       },
+      body: JSON.stringify(newPostData),
+    });
+
+    const data = await response.json().catch(() => ({})); // never fails
+
+    if (!response.ok) {
+      const message = data.error || data.message || "Failed to create post";
+      throw new Error(message);
+    }
+
+     // Fetch updated posts list
+    const eventsResponse = await fetch("http://localhost:3001/api/posts", {
+      headers: { 'Authorization': `Bearer ${token}` }
+    });
+    const updatedPosts = await eventsResponse.json();
+
+    setPosts (updatedPosts); // update parent
+
+    console.log("Post created successfully:", data);
+
+    // Reset form
+    setTitle('');
+    setDescription('');
+    
+
+    // Update local state to include the new event
+   setPosts(prev => [...prev, data]);
+ 
+  } catch (error) {
+    console.error("Event creation failed", error.message);
+    displayError(error.message);
+  }
+};
+
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-lg mx-auto bg-white p-6 rounded-lg shadow-md space-y-4">
+      <h1> ✏️ Create a new Post</h1>
+      
+      <div >
+        <label htmlFor="title" className="block text-gray-700">Event Name</label>
+        <input
+          id="title"
+          type="text"
+          placeholder="Enter event name"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        />
+      </div>
+
+      <div className="space-y-1">
+        <label htmlFor="description" className="block text-gray-700">Description</label>
+        <textarea
+          id="description"
+          placeholder="Enter event description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+          rows="4"
+          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        ></textarea>
+      </div>
+
+      <div className="space-y-1">
+        <label htmlFor="type" className="block text-gray-700">Type</label>
+        <select
+          id="type"
+          value={type}
+          onChange={(e) => setEventType(e.target.value)}
+          required
+          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+          <option value="">Select a Type</option>
+          <option value="online">Online</option>
+          <option value="in-person">In-person</option>
+         
+        </select>
+      </div>
+
+
+ {type === "in-person" && (
+      <div className="space-y-1">
+        <label htmlFor="location" className="block text-gray-700">Location</label>
+        <input
+          id="location"
+          type="text"
+          placeholder="Enter event location"
+          value={location}
+          onChange={(e) => setEventLocation(e.target.value)}
+          required={type === "in-person"} //only if type is in person
+          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        ></input>
+      </div>
+ )}
+
+{type === "online" && (
+        <div>
+          <label htmlFor="link" className="block text-gray-700">
+            Event Link
+          </label>
+          <input
+            id="link"
+            type="url"
+            value={link}
+            onChange={(e) => setEventLink(e.target.value)}
+            placeholder="https://example.com/event"
+            required={type === "online"}
+            className="w-full px-4 py-2 border rounded-md"
+          />
+        </div>
+      )}
+
+ <div>
+        <label htmlFor="eventDate" className="block text-gray-700">
+          Date
+        </label>
+        <input
+          id="eventDate"
+          type="date"
+          value={eventDate}
+          onChange={(e) => setEventDate(e.target.value)}
+          required
+          className="w-full px-4 py-2 border rounded-md"
+        />
+      </div>
+      
+      <div>
+
+       <label htmlFor="eventTime" className="block text-gray-700">
+          Time
+        </label>
+        <input
+          id="eventTime"
+          type="time"
+          value={eventTime}
+          onChange={(e) => setEventTime(e.target.value)}
+          required
+          className="w-full px-4 py-2 border rounded-md"
+        />
+      </div>
+         
+   
+
+      {error && <p className="text-red-500 text-center">{error}</p>}
+      
+      <button
+        type="submit"
+        className="w-full bg-[var(--primary)] text-white font-bold py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        Create Event
+      </button>
+    </form>
+  );
+};
+
+export default CreateEvent;
